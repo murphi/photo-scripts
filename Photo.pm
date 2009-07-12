59d532c83acfec3eb3b622af2a0ed41d8b6e74e5
Martin Engelmann <martin.engelmann@gmx.de>
2009-07-12 19:45:43 +0200

    Photo: implemented getDifference

--- Photo.pm
+++ Photo.pm
@@ -1,21 +1,50 @@
 package Photo;
 require Exporter;
 our @ISA    = qw( Exporter );
-our @EXPORT = qw( getCreateDate );
+our @EXPORT = qw( getCreateDate setCreateDate getDifference);
+our $VERSION = 0.01;
 
 use strict;
 use warnings;
 use Image::ExifTool qw(:Public);
+use DateTime::Format::Exif;
 
 sub getCreateDate
 {
 	my $file = shift;
-	return undef unless -r $file;
+	die "$file is not readable" unless -r $file;
 
 	my $exif = new Image::ExifTool;
 	$exif->ExtractInfo($file);
-	return $exif->GetValue('CreateDate');
+	return 
+		DateTime::Format::Exif->parse_datetime(
+			$exif->GetValue('CreateDate')
+		);
 }
 
+
+sub setCreateDate
+{
+	my ($file, $dt) = @_;
+	die "No file specified." unless defined $file;
+	die "$file is not writeable." unless -w $file;
+	die "No date given." unless defined $dt;
+
+	my $exif = new Image::ExifTool;
+	$exif->SetNewValue('CreateDate', $dt);
+	$exif->WriteInfo($file);
+}
+
+
+sub getDifference
+{
+	my ($file, $time) = @_;
+	die "$file is not readable." unless -r $file;
+
+	my $parser = DateTime::Format::ISO8601->new();
+	my $reference = $parser->parse_datetime($time);
+
+	return getCreateDate($file) - $reference;
+}
 
 
