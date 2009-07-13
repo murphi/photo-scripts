03e7de90d9b237b8d9f43f60346024b6e6bec03a
Martin Engelmann <martin.engelmann@gmx.de>
2009-07-13 20:27:47 +0200

    Added license text

--- Photo.pm
+++ Photo.pm
@@ -4,10 +4,28 @@ our @ISA    = qw( Exporter );
 our @EXPORT = qw( getCreateDate setCreateDate getDifference);
 our $VERSION = 0.01;
 
+# photo-scripts - Set of scripts to edit a collection of photos
+# Copyright (C) 2009, Martin Engelmann <martin.engelmann@gmx.de>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 use strict;
 use warnings;
 use Image::ExifTool qw(:Public);
 use DateTime::Format::Exif;
+use DateTime::Format::ISO8601;
 
 sub getCreateDate
 {
--- adjust-time.pl
+++ adjust-time.pl
@@ -1,11 +1,28 @@
 #!/usr/bin/perl -w
-#use strict;
+
+# photo-scripts - Set of scripts to edit a collection of photos
+# Copyright (C) 2009, Martin Engelmann <martin.engelmann@gmx.de>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use strict;
 
 use Photo;
 use Image::ExifTool qw(:Public);
 use Date::Parse;
 use DateTime::Format::Exif;
-use DateTime::Format::ISO8601;
 use DateTime::Format::Duration;
 
 my $formatDuration = DateTime::Format::Duration->new(pattern => "%r");
